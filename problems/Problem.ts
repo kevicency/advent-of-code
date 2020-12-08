1176e12f0edb4bc57d26a06c36c5efda5d3cf458
Kevin Mees <kev.mees@gmail.com>
2020-12-08 22:30:01 +0100

    solve day03

--- problems/Problem.ts
+++ problems/Problem.ts
@@ -2,5 +2,7 @@ export interface Problem {
   input: any
   description: string
   solve(): any
+  solve(input: any, ...args: any): any
   solveBonus(): any
+  solveBonus(input: any, ...args: any): any
 }
