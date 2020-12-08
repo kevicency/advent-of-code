9fb28a8c9e5c01cd6e390422d585b50b275a6e74
Kevin Mees <kev.mees@gmail.com>
2020-12-08 21:35:32 +0100

    add problem template

---
+++ problems/00_TEMPLATE/index.ts
@@ -0,0 +1,13 @@
+import { Problem } from '../Problem'
+import inputTxt from './input.txt'
+import description from './PROBLEM.md'
+
+const input = inputTxt.split('\n')
+
+export default {
+  input,
+  description,
+
+  solve: () => void 0,
+  solveBonus: () => void 0,
+} as Problem
